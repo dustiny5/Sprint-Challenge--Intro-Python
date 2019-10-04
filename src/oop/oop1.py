da6ad3fc503c9a70da70f1ac2f9c9f727c5345eb
dustiny5 <dustiny5@users.noreply.github.com>
2019-10-04 08:29:39 -0700

    Added drive method to both classes: Overid drive() in 2nd class and printed results

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,31 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Base/Main Class
+class Vehicle:
+    pass
+
+# Inherits from Vehicle
+class FlightVehicle(Vehicle):
+    pass
+
+# Inherits from FlightVehicle which inherits from Vehicle
+class Starship(FlightVehicle):
+    pass
+
+# Inherits from FlightVehicle which inherits from Vehicle
+class Airplane(FlightVehicle):
+    pass
+
+# Inherits from Vehicle
+class GroundVehicle(Vehicle):
+    pass
+
+# Inherits from GroundVehicle which inherits from Vehicle
+class Car(GroundVehicle):
+    pass
+
+# Inherits from GroundVehicle which inherits from Vehicle
+class Motorcycle(GroundVehicle):
+    pass
